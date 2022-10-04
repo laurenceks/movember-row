bde4fd524dbe73d91c446c2ae05862169527d190
Laurence Summers <69108396+laurenceks@users.noreply.github.com>
2022-10-04 16:13:27 +0100

    Adds CSS minifying and purging

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,15 @@
 const path = require("path");
+const glob = require("glob-all");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const PurgecssPlugin = require("purgecss-webpack-plugin");
 
 module.exports = {
     entry: "./src/index.js",
     output: {
         filename: "main.js",
         path: path.resolve(__dirname, "dist"),
+        clean: true,
     },
     devServer: {
         static: {
@@ -16,12 +20,23 @@ module.exports = {
         liveReload: true,
         hot: false,
     },
-    plugins: [new HtmlWebpackPlugin({template: "public/index.html"})],
+    plugins: [
+        new PurgecssPlugin({
+            paths: glob.sync([`${path.join(__dirname, "src")}/**/*`,`${path.join(__dirname, "dist")}/**/*`,`${path.join(__dirname, "public")}/**/*`], {nodir: true}),
+        }),
+        new HtmlWebpackPlugin({template: "public/index.html"}),
+        new MiniCssExtractPlugin({filename: "[name].css"}),
+    ],
     mode: "development",
     module: {
         rules: [{
             test: /\.css$/i,
-            use: ["style-loader", "css-loader"],
+            use: [MiniCssExtractPlugin.loader, "css-loader"],
+        }, {
+            test: /\.s[ac]ss$/i,
+            exclude: /node_modules/,
+            use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
         }],
     },
+
 };
