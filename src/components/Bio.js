f3a3af46e7ccf7ce5eca81b3135674597c2e7265
Laurence Summers <69108396+laurenceks@users.noreply.github.com>
2022-10-21 09:53:53 +0100

    Adds custom Bio component

---
+++ src/components/Bio.js
@@ -0,0 +1,18 @@
+export default class Bio extends HTMLElement {
+    connectedCallback() {
+        const vars = this.attributes;
+        this.innerHTML = `<div class="peopleWrap">
+            <div class="peopleContainer d-flex flex-wrap flex-sm-nowrap justify-content-center justify-content-sm-start align-items-center">
+                <div class="text-center img-circle-container">
+                    <div class="img-circle-wrap">
+                        <img class="img-circle" ${
+                            vars.src ? `src="${vars.src.value}"` : ""
+                        } alt=""/>
+                    </div>
+                    <h3 class="my-3">${vars.name.value}</h3>
+                </div>
+                <p class="p-3 m-0 mb-sm-5">${vars.body.value}</p>
+            </div>
+        </div>`;
+    }
+}
