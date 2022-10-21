6aa8cb04b9c8c8d1531cdd4fa3dab89f9268b134
Laurence Summers <69108396+laurenceks@users.noreply.github.com>
2022-10-21 09:55:43 +0100

    Adds dynamic creation of rower bios using a custom HTML element rower-bios

--- src/index.js
+++ src/index.js
@@ -4,17 +4,25 @@ import getCookieConsent from "./js/getCookieConsent";
 import fetchGoogleSheetsData from "./js/fetchGoogleSheetsData";
 import setTables from "./js/setTables";
 import initMap from "./js/initMap";
+import setBios from "./js/setBios";
 
-// TODO - hide # from URL
+// TODO - hide # from URL ?
+
 /*
 window.addEventListener("hashchange", (e) => {
-    history.pushState(null,null,window.location.href.replace("#",""));
+    history.pushState(
+        "",
+        document.title,
+        e.newURL.match(/#[/\-\w]+/)[0]?.replace("#", "")
+    );
 });
 */
 
 const init = async () => {
     const mapAnimationDurationInMs = 3000;
     const sheetsData = await fetchGoogleSheetsData();
+
+    setBios();
 
     // populate tables
     setTables(sheetsData);
---
+++ src/js/setBios.js
@@ -0,0 +1,78 @@
+import Bio from "../components/Bio";
+
+const setBios = () => {
+    const bios = [
+        {
+            name: "Sam",
+            imgSrc: "/img/rower-sam.jpg",
+            body: "Paramedic at Tongham, fifth year in the team. Rowing for Mayes, Bailey and Eric. “I started participating after my grandfather was diagnosed with prostate cancer, since then Movember’s involvement with men’s mental health has taken my focus. My father had depression when I was younger but it’s only within the past few years that I understood what he was going through. More recently one of my close friends lost her brother to suicide after losing his battle with mental health.”",
+        },
+        {
+            name: "Oli R",
+            imgSrc: "/img/rower-oli.jpg",
+            body: "Paramedic at Tongham, fifth year in the team. Rowing for Gramps and anyone who's lost someone to or suffers with mental health challenges. Last year Oli took part in the 60km for Movember and this year feels this challenge will really test the team’s limits and “push the boat out” as well as get people talking about mental health. It’s one thing to grow a moustache (though some of the team find it harder than others) but this year these paramedics wanted a real challenge.",
+        },
+        {
+            name: "Pete",
+            body: "Critical Care Paramedic at Tongham, fourth year in the team.",
+        },
+        {
+            name: "Stan",
+            imgSrc: "/img/rower-stan.jpg",
+            body: "Paramedic/Clinical Supervisor at Chertsey/Crawley, third year in the team.",
+        },
+        {
+            name: "Will",
+            imgSrc: "/img/rower-will.jpg",
+            body: "Paramedic at Chertsey, second year in the team.",
+        },
+        {
+            name: "Oli W",
+            imgSrc: "/img/rower-oli-w.jpg",
+            body: "Paramedic at Chertsey, second year in the team.",
+        },
+        {
+            name: "Nathan",
+            imgSrc: "/img/rower-nathan.jpeg",
+            body: "Paramedic at Chertsey, second year in the team.",
+        },
+        {
+            name: "Ian",
+            imgSrc: "/img/rower-ian.jpeg",
+            body: "Paramedic at Godalming, third year in the team.",
+        },
+        {
+            name: "Sam G",
+            imgSrc: "/img/rower-sam-g.jpeg",
+            body: "Operations Manager at Chertsey, second year in the team.",
+        },
+        {
+            name: "Chris",
+            body: "Operational Team Leader at Tongham, second year in the team.",
+        },
+        {
+            name: "Ben",
+            imgSrc: "/img/rower-ben.jpg",
+            body: "Critical Care Paramedic at Chertsey, first year in the team.",
+        },
+        {
+            name: "Harry",
+            imgSrc: "/img/rower-harry.jpeg",
+            body: "Paramedic at Chertsey, first year in the team.",
+        },
+    ];
+
+    customElements.define("rower-bio", Bio);
+
+    bios.forEach((x) => {
+        const newBio = document.createElement("rower-bio");
+        if (x.imgSrc) {
+            newBio.setAttribute("src", x.imgSrc);
+        }
+        newBio.setAttribute("name", x.name);
+        newBio.setAttribute("body", x.body);
+        document.getElementById("bioContainer").appendChild(newBio);
+    });
+};
+
+export default setBios;
