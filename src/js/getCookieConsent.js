4c95028347a7bbb2f20a4a335a6da00f77a72f42
Laurence Summers <69108396+laurenceks@users.noreply.github.com>
2022-11-09 15:45:44 +0000

    Sets additional cookie if I agree to all button is clicked and injects Google Analytics script

--- src/js/getCookieConsent.js
+++ src/js/getCookieConsent.js
@@ -1,5 +1,25 @@
 import Cookies from "js-cookie";
-import { Modal } from "bootstrap";
+import {Modal} from "bootstrap";
+import {googleTag} from "./data/tokens";
+
+const startGoogleAnalytics = () => {
+    //set Google analytics here
+    //https://stackoverflow.com/questions/69685735/how-to-load-an-external-javascript-on-a-condition
+    //https://github.com/DavidWells/analytics/blob/master/packages/analytics-plugin-google-analytics/src/browser.js
+    const script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.async = true;
+    script.src = `https://www.googletagmanager.com/gtag/js?id=${googleTag}`;
+    document.head.append(script);
+    window.dataLayer = window.dataLayer || [];
+
+    function gtag() {
+        dataLayer.push(arguments);
+    }
+
+    gtag('js', new Date());
+    gtag('config', googleTag);
+};
 
 const getCookieConsent = (callback) => {
     if (!Cookies.get("cookie-consent-given")) {
@@ -10,15 +30,25 @@ const getCookieConsent = (callback) => {
             keyboard: false,
         });
         cookieModal.show();
-        cookieModalElement.addEventListener("hide.bs.modal", () => {
+        cookieModalElement.querySelectorAll("button").forEach((x) => x.addEventListener("click", (e) => {
+            if (e.target.dataset["cookieBtn"] === "all") {
+                Cookies.set("cookie-consent-given-all", true, {
+                    expires: 30,
+                    sameSite: "strict",
+                });
+                startGoogleAnalytics();
+            }
             Cookies.set("cookie-consent-given", true, {
                 expires: 30,
                 sameSite: "strict",
             });
             callback();
-        });
+        }));
     } else {
         callback();
+        if(Cookies.get("cookie-consent-given-all")){
+            startGoogleAnalytics();
+        }
     }
 };
 
